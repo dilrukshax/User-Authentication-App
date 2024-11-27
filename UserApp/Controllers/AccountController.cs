308ed86d3f2e36e9c9e5d1aef85e673aa1d4beb0
Dilan Dilruksha <100220079+dilrukshax@users.noreply.github.com>
2024-11-27 13:34:23 +0530

    Create email verification page

--- UserApp/Controllers/AccountController.cs
+++ UserApp/Controllers/AccountController.cs
@@ -13,5 +13,10 @@ namespace UserApp.Controllers
         {
             return View();  
         }
+
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
     }
 }
