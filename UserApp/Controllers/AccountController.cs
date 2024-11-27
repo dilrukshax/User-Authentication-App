cabb05c8012f8131ae290c2e92056b4503625286
Dilan Dilruksha <100220079+dilrukshax@users.noreply.github.com>
2024-11-27 13:56:40 +0530

    Create registration method / action

--- UserApp/Controllers/AccountController.cs
+++ UserApp/Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserApp.Models;
+using UserApp.ViewModels;
 
 namespace UserApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -12,6 +24,37 @@ namespace UserApp.Controllers
         public IActionResult Register()
         {
             return View();  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users user = new Users
+                {
+                    Fullname = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return View(model);
+                }                
+            }
+            return View(model);
         }
 
         public IActionResult VerifyEmail()
