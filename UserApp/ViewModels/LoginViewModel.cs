e846100d6776cb855d065818e228983d2a43d57e
Dilan Dilruksha <100220079+dilrukshax@users.noreply.github.com>
2024-11-27 10:55:23 +0530

    modify changes

--- UserApp/ViewModels/LoginViewModel.cs
+++ UserApp/ViewModels/LoginViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace UserApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserApp.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
     }
 }
--- UserApp/ViewModels/VerifyEmailViewModel.cs
+++ UserApp/ViewModels/VerifyEmailViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace UserApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserApp.ViewModels
 {
     public class VerifyEmailViewModel
     {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
     }
 }
